6707871e92e161365146bd1eec9de5d0e900e44b
Edgar Rokian <edgar.rokyan@gmail.com>
2018-05-16 23:58:16 +0200

    Add noexcept for transparent functors and make their function call operator SFINAE-friendly. Formatting changes.

--- include/cpputil/basic_functors.h
+++ include/cpputil/basic_functors.h
@@ -2,110 +2,140 @@
 
 #include "forward.h"
 
-namespace cpputil {
+namespace cpputil
+{
 
     struct transparent_tag {};
+
+    struct transparent_functor
+    {
+        using is_transparent = transparent_tag;
+    };
 
     // Arithmetic operations
 
     template<typename T = void>
-    struct plus {
-        constexpr T operator()(const T& x, const T& y) const {
+    struct plus
+    {
+        constexpr T operator()(const T& x, const T& y) const
+        {
             return x + y;
         }
     };
 
     template<>
-    struct plus<void> {
-        using is_transparent = transparent_tag;
-
+    struct plus<void> : transparent_functor
+    {
         template<typename T, typename U>
-        constexpr decltype(auto) operator()(T&& x, U&& y) const {
+        constexpr auto operator()(T&& x, U&& y) const
+            noexcept(noexcept(cpputil::forward<T>(x) + cpputil::forward<U>(y)))
+            -> decltype(cpputil::forward<T>(x) + cpputil::forward<U>(y))
+        {
             return (cpputil::forward<T>(x) + cpputil::forward<U>(y));
         }
     };
 
     template<typename T = void>
-    struct minus {
-        constexpr T operator()(const T& x, const T& y) const {
+    struct minus
+    {
+        constexpr T operator()(const T& x, const T& y) const
+        {
             return x - y;
         }
     };
 
     template<>
-    struct minus<void> {
-        using is_transparent = transparent_tag;
-
+    struct minus<void> : transparent_functor
+    {
         template<typename T, typename U>
-        constexpr decltype(auto) operator()(T&& x, U&& y) const {
+        constexpr auto operator()(T&& x, U&& y) const
+            noexcept(noexcept(cpputil::forward<T>(x) - cpputil::forward<U>(y)))
+            -> decltype(cpputil::forward<T>(x) - cpputil::forward<U>(y))
+        {
             return (cpputil::forward<T>(x) - cpputil::forward<U>(y));
         }
     };
 
     template<typename T = void>
-    struct multiplies {
-        constexpr T operator()(const T& x, const T& y) const {
+    struct multiplies
+    {
+        constexpr T operator()(const T& x, const T& y) const
+        {
             return x * y;
         }
     };
 
     template<>
-    struct multiplies<void> {
-        using is_transparent = transparent_tag;
-
+    struct multiplies<void> : transparent_functor
+    {
         template<typename T, typename U>
-        constexpr decltype(auto) operator()(T&& x, U&& y) const {
+        constexpr auto operator()(T&& x, U&& y) const
+            noexcept(noexcept(cpputil::forward<T>(x) * cpputil::forward<U>(y)))
+            -> decltype(cpputil::forward<T>(x) * cpputil::forward<U>(y))
+        {
             return (cpputil::forward<T>(x) * cpputil::forward<U>(y));
         }
     };
 
     template<typename T = void>
-    struct divides {
-        constexpr T operator()(const T& x, const T& y) const {
+    struct divides
+    {
+        constexpr T operator()(const T& x, const T& y) const
+        {
             return x / y;
         }
     };
 
     template<>
-    struct divides<void> {
-        using is_transparent = transparent_tag;
-
+    struct divides<void> : transparent_functor
+    {
         template<typename T, typename U>
-        constexpr decltype(auto) operator()(T&& x, U&& y) const {
+        constexpr auto operator()(T&& x, U&& y) const
+            noexcept(noexcept(cpputil::forward<T>(x) / cpputil::forward<U>(y)))
+            -> decltype(cpputil::forward<T>(x) / cpputil::forward<U>(y))
+        {
             return (cpputil::forward<T>(x) / cpputil::forward<U>(y));
         }
     };
 
     template<typename T = void>
-    struct modulus {
-        constexpr T operator()(const T& x, const T& y) const {
+    struct modulus
+    {
+        constexpr T operator()(const T& x, const T& y) const
+        {
             return x % y;
         }
     };
 
     template<>
-    struct modulus<void> {
-        using is_transparent = transparent_tag;
-
+    struct modulus<void> : transparent_functor
+    {
         template<typename T, typename U>
-        constexpr decltype(auto) operator()(T&& x, U&& y) const {
+        constexpr auto operator()(T&& x, U&& y) const
+            noexcept(noexcept(cpputil::forward<T>(x) % cpputil::forward<U>(y)))
+            -> decltype(cpputil::forward<T>(x) % cpputil::forward<U>(y))
+        {
             return (cpputil::forward<T>(x) % cpputil::forward<U>(y));
         }
     };
 
     template<typename T = void>
-    struct negate {
-        constexpr T operator()(const T& x) const {
+    struct negate
+    {
+        constexpr T operator()(const T& x) const
+        {
             return -x;
         }
     };
 
     template<>
-    struct negate<void> {
-        using is_transparent = transparent_tag;
-
+    struct negate<void> : transparent_functor
+    {
         template<typename T>
-        constexpr decltype(auto) operator()(T&& x) const {
+        constexpr auto operator()(T&& x) const
+            noexcept(noexcept(-cpputil::forward<T>(x)))
+            -> decltype(-cpputil::forward<T>(x))
+        {
             return -cpputil::forward<T>(x);
         }
     };
@@ -113,103 +143,127 @@ namespace cpputil {
     // Comparison operations
 
     template<typename T = void>
-    struct equal_to {
-        constexpr bool operator()(const T& x, const T& y) const {
+    struct equal_to
+    {
+        constexpr bool operator()(const T& x, const T& y) const
+        {
             return (x == y);
         }
      };
 
     template<>
-    struct equal_to<void> {
-        using is_transparent = transparent_tag;
-
+    struct equal_to<void> : transparent_functor
+    {
         template<typename T, typename U>
-        constexpr decltype(auto) operator()(T&& x, U&& y) const {
+        constexpr auto operator()(T&& x, U&& y) const
+            noexcept(noexcept(cpputil::forward<T>(x) == cpputil::forward<U>(y)))
+            -> decltype(cpputil::forward<T>(x) == cpputil::forward<U>(y))
+        {
             return (cpputil::forward<T>(x) == cpputil::forward<U>(y));
         }
     };
 
     template<typename T = void>
-    struct not_equal_to {
-        constexpr bool operator()(const T& x, const T& y) const {
+    struct not_equal_to
+    {
+        constexpr bool operator()(const T& x, const T& y) const
+        {
             return (x != y);
         }
     };
 
     template<>
-    struct not_equal_to<void> {
-        using is_transparent = transparent_tag;
-
+    struct not_equal_to<void> : transparent_functor
+    {
         template<typename T, typename U>
-        constexpr decltype(auto) operator()(T&& x, U&& y) const {
+        constexpr auto operator()(T&& x, U&& y) const
+            noexcept(noexcept(cpputil::forward<T>(x) != cpputil::forward<U>(y)))
+            -> decltype(cpputil::forward<T>(x) != cpputil::forward<U>(y))
+        {
             return (cpputil::forward<T>(x) != cpputil::forward<U>(y));
         }
     };
 
     template<typename T = void>
-    struct greater {
-        constexpr bool operator()(const T& x, const T& y) const {
+    struct greater
+    {
+        constexpr bool operator()(const T& x, const T& y) const
+        {
             return (x > y);
         }
     };
 
     template<>
-    struct greater<void> {
-        using is_transparent = transparent_tag;
-
+    struct greater<void> : transparent_functor
+    {
         template<typename T, typename U>
-        constexpr decltype(auto) operator()(T&& x, U&& y) const {
+        constexpr auto operator()(T&& x, U&& y) const
+            noexcept(noexcept(cpputil::forward<T>(x) > cpputil::forward<U>(y)))
+            -> decltype(cpputil::forward<T>(x) > cpputil::forward<U>(y))
+        {
             return (cpputil::forward<T>(x) > cpputil::forward<U>(y));
         }
     };
 
     template<typename T = void>
-    struct less {
-        constexpr bool operator()(const T& x, const T& y) const {
+    struct less
+    {
+        constexpr bool operator()(const T& x, const T& y) const
+        {
             return (x < y);
         }
     };
 
     template<>
-    struct less<void> {
-        using is_transparent = transparent_tag;
-
+    struct less<void> : transparent_functor
+    {
         template<typename T, typename U>
-        constexpr decltype(auto) operator()(T&& x, U&& y) const {
+        constexpr auto operator()(T&& x, U&& y) const
+            noexcept(noexcept(cpputil::forward<T>(x) < cpputil::forward<U>(y)))
+            -> decltype(cpputil::forward<T>(x) < cpputil::forward<U>(y))
+        {
             return (cpputil::forward<T>(x) < cpputil::forward<U>(y));
         }
     };
 
     template<typename T = void>
-    struct greater_equal {
-        constexpr bool operator()(const T& x, const T& y) const {
+    struct greater_equal
+    {
+        constexpr bool operator()(const T& x, const T& y) const
+        {
             return (x >= y);
         }
     };
 
     template<>
-    struct greater_equal<void> {
-        using is_transparent = transparent_tag;
-
+    struct greater_equal<void> : transparent_functor
+    {
         template<typename T, typename U>
-        constexpr decltype(auto) operator()(T&& x, U&& y) const {
+        constexpr auto operator()(T&& x, U&& y) const
+            noexcept(noexcept(cpputil::forward<T>(x) >= cpputil::forward<U>(y)))
+            -> decltype(cpputil::forward<T>(x) >= cpputil::forward<U>(y))
+        {
             return (cpputil::forward<T>(x) >= cpputil::forward<U>(y));
         }
     };
 
     template<typename T = void>
-    struct less_equal {
-        constexpr bool operator()(const T& x, const T& y) const {
+    struct less_equal
+    {
+        constexpr bool operator()(const T& x, const T& y) const
+        {
             return (x <= y);
         }
     };
 
     template<>
-    struct less_equal<void> {
-        using is_transparent = transparent_tag;
-
+    struct less_equal<void> : transparent_functor
+    {
         template<typename T, typename U>
-        constexpr decltype(auto) operator()(T&& x, U&& y) const {
+        constexpr auto operator()(T&& x, U&& y) const
+            noexcept(noexcept(cpputil::forward<T>(x) <= cpputil::forward<U>(y)))
+            -> decltype(cpputil::forward<T>(x) <= cpputil::forward<U>(y))
+        {
             return (cpputil::forward<T>(x) <= cpputil::forward<U>(y));
         }
     };
@@ -217,52 +271,64 @@ namespace cpputil {
     // Logical operations
 
     template<typename T = void>
-    struct logical_and {
-        constexpr bool operator()(const T& x, const T& y) const {
+    struct logical_and
+    {
+        constexpr bool operator()(const T& x, const T& y) const
+        {
             return (x && y);
         }
     };
 
     template<>
-    struct logical_and<void> {
-        using is_transparent = transparent_tag;
-
+    struct logical_and<void> : transparent_functor
+    {
         template<typename T, typename U>
-        constexpr decltype(auto) operator()(T&& x, U&& y) const {
+        constexpr auto operator()(T&& x, U&& y) const
+            noexcept(noexcept(cpputil::forward<T>(x) && cpputil::forward<U>(y)))
+            -> decltype(cpputil::forward<T>(x) && cpputil::forward<U>(y))
+        {
             return (cpputil::forward<T>(x) && cpputil::forward<U>(y));
         }
     };
 
     template<typename T = void>
-    struct logical_or {
-        constexpr bool operator()(const T& x, const T& y) const {
+    struct logical_or
+    {
+        constexpr bool operator()(const T& x, const T& y) const
+        {
             return (x || y);
         }
     };
 
     template<>
-    struct logical_or<void> {
-        using is_transparent = transparent_tag;
-
+    struct logical_or<void> : transparent_functor
+    {
         template<typename T, typename U>
-        constexpr decltype(auto) operator()(T&& x, U&& y) const {
+        constexpr auto operator()(T&& x, U&& y) const
+            noexcept(noexcept(cpputil::forward<T>(x) || cpputil::forward<U>(y)))
+            -> decltype(cpputil::forward<T>(x) || cpputil::forward<U>(y))
+        {
             return (cpputil::forward<T>(x) || cpputil::forward<U>(y));
         }
     };
 
     template<typename T = void>
-    struct logical_not {
-        constexpr bool operator()(const T& x) const {
+    struct logical_not
+    {
+        constexpr bool operator()(const T& x) const
+        {
             return !x;
         }
     };
 
     template<>
-    struct logical_not<void> {
-        using is_transparent = transparent_tag;
-
+    struct logical_not<void> : transparent_functor
+    {
         template<typename T>
-        constexpr decltype(auto) operator()(T&& x) const {
+        constexpr auto operator()(T&& x) const
+            noexcept(noexcept(!cpputil::forward<T>(x)))
+            -> decltype(!cpputil::forward<T>(x))
+        {
             return !cpputil::forward<T>(x);
         }
     };
@@ -270,69 +336,85 @@ namespace cpputil {
     // Bitwise operations
 
     template<typename T = void>
-    struct bit_and {
-        constexpr T operator()(const T& x, const T& y) const {
+    struct bit_and
+    {
+        constexpr T operator()(const T& x, const T& y) const
+        {
             return (x & y);
         }
     };
 
     template<>
-    struct bit_and<void> {
-        using is_transparent = transparent_tag;
-
+    struct bit_and<void> : transparent_functor
+    {
         template<typename T, typename U>
-        constexpr decltype(auto) operator()(T&& x, U&& y) const {
+        constexpr auto operator()(T&& x, U&& y) const
+            noexcept(noexcept(cpputil::forward<T>(x) & cpputil::forward<U>(y)))
+            -> decltype(cpputil::forward<T>(x) & cpputil::forward<U>(y))
+        {
             return (cpputil::forward<T>(x) & cpputil::forward<U>(y));
         }
     };
 
     template<typename T = void>
-    struct bit_or {
-        constexpr T operator()(const T& x, const T& y) const {
+    struct bit_or
+    {
+        constexpr T operator()(const T& x, const T& y) const
+        {
             return (x | y);
         }
     };
 
     template<>
-    struct bit_or<void> {
-        using is_transparent = transparent_tag;
-
+    struct bit_or<void> : transparent_functor
+    {
         template<typename T, typename U>
-        constexpr decltype(auto) operator()(T&& x, U&& y) const {
+        constexpr auto operator()(T&& x, U&& y) const
+            noexcept(noexcept(cpputil::forward<T>(x) | cpputil::forward<U>(y)))
+            -> decltype(cpputil::forward<T>(x) | cpputil::forward<U>(y))
+        {
             return (cpputil::forward<T>(x) | cpputil::forward<U>(y));
         }
     };
 
     template<typename T = void>
-    struct bit_xor {
-        constexpr T operator()(const T& x, const T& y) const {
+    struct bit_xor
+    {
+        constexpr T operator()(const T& x, const T& y) const
+        {
             return (x ^ y);
         }
     };
 
     template<>
-    struct bit_xor<void> {
-        using is_transparent = transparent_tag;
-
+    struct bit_xor<void> : transparent_functor
+    {
         template<typename T, typename U>
-        constexpr decltype(auto) operator()(T&& x, U&& y) const {
+        constexpr auto operator()(T&& x, U&& y) const
+            noexcept(noexcept(cpputil::forward<T>(x) ^ cpputil::forward<U>(y)))
+            -> decltype(cpputil::forward<T>(x) ^ cpputil::forward<U>(y))
+        {
             return (cpputil::forward<T>(x) ^ cpputil::forward<U>(y));
         }
     };
 
     template<typename T = void>
-    struct bit_not {
-        constexpr T operator()(const T& x) const {
+    struct bit_not
+    {
+        constexpr T operator()(const T& x) const
+        {
             return ~x;
         }
     };
 
     template<>
-    struct bit_not<void> {
-        using is_transparent = transparent_tag;
-
+    struct bit_not<void> : transparent_functor
+    {
         template<typename T>
-        constexpr decltype(auto) operator()(T&& x) const {
+        constexpr auto operator()(T&& x) const
+            noexcept(noexcept(~cpputil::forward<T>(x)))
+            -> decltype(~cpputil::forward<T>(x))
+        {
             return ~cpputil::forward<T>(x);
         }
     };
