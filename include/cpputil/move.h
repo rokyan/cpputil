03aaed0461a04a99ed5af2585e92fd13661d51ef
Edgar Rokian <edgar.rokyan@gmail.com>
2018-06-02 13:19:18 +0200

    Add move_if_noexcept with tests.

--- include/cpputil/move.h
+++ include/cpputil/move.h
@@ -9,4 +9,17 @@ namespace cpputil
     {
         return static_cast<std::remove_reference_t<T>&&>(t);
     }
+
+    namespace detail
+    {
+        template<typename T>
+        inline constexpr auto move_if_noexcept_condition_v =
+            !std::is_nothrow_move_constructible_v<T> && std::is_copy_constructible_v<T>;
+    }
+
+    template<typename T>
+    constexpr std::conditional_t<detail::move_if_noexcept_condition_v<T>, const T&, T&&> move_if_noexcept(T& t) noexcept
+    {
+        return move(t);
+    }
 }
