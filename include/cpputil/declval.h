51f3274f726f0afe0dc03284813ccccd5df79035
Edgar Rokian <edgar.rokyan@gmail.com>
2018-06-03 12:49:17 +0200

    Add declval protector.

--- include/cpputil/declval.h
+++ include/cpputil/declval.h
@@ -4,6 +4,20 @@
 
 namespace cpputil
 {
+    namespace detail
+    {
+        template<typename T>
+        struct declval_protector
+        {
+            static constexpr auto always_false = false;
+            static std::add_rvalue_reference_t<T> delegate();
+        };
+    }
+
     template<typename T>
-    std::add_rvalue_reference_t<T> declval() noexcept;
+    std::add_rvalue_reference_t<T> declval() noexcept
+    {
+        static_assert(detail::declval_protector<T>::always_false, "declval() must not be called!");
+        return detail::declval_protector<T>::delegate();
+    }
 }
