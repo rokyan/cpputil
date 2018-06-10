86a8d70d2593ec38481687fa529939ce7da623fd
Edgar Rokian <edgar.rokyan@gmail.com>
2018-06-10 23:48:09 +0200

    Add missing includes to cpputil.h.

--- include/cpputil.h
+++ include/cpputil.h
@@ -4,6 +4,8 @@
 #include "cpputil\operator_functors.h"
 #include "cpputil\not_fn.h"
 #include "cpputil\apply.h"
+#include "cpputil\as_const.h"
 #include "cpputil\exchange.h"
+#include "cpputil\declval.h"
 #include "cpputil\forward.h"
 #include "cpputil\move.h"
