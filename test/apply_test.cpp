799c97aada39fd75aaaf70ae364fe3026838b40e
Edgar Rokian <edgar.rokyan@gmail.com>
2018-06-10 23:28:40 +0200

    Expand apply test.

--- test/apply_test.cpp
+++ test/apply_test.cpp
@@ -5,8 +5,13 @@ namespace test
 {
     TEST(apply_test, test_apply)
     {
-        const auto callable = [](int x, int y) { return x + y; };
+        const auto unary_callable =[](int x) { return x * x; };
+        EXPECT_EQ(cpputil::apply(unary_callable, std::make_tuple(2)), 2 * 2);
 
-        EXPECT_EQ(cpputil::apply(callable, std::make_tuple(1, 2)), 3);
+        const auto binary_callable = [](int x, int y) { return x * x + y * y; };
+        EXPECT_EQ(cpputil::apply(binary_callable, std::make_tuple(2, 4)), 2 * 2 + 4 * 4);
+
+        const auto ternary_callable = [](int x, int y, int z) { return x * x + y * y + z * z; };
+        EXPECT_EQ(cpputil::apply(ternary_callable, std::make_tuple(2, 4, 8)), 2 * 2 + 4 * 4 + 8 * 8);
     }
 }
