4ad9d88595cb7299a6108f3c572d4234de50b459
Edgar Rokian <edgar.rokyan@gmail.com>
2018-06-10 23:35:56 +0200

    Add apply.h to cpputil.h.

--- include/cpputil.h
+++ include/cpputil.h
@@ -3,6 +3,7 @@
 #include "cpputil\reference_wrapper.h"
 #include "cpputil\operator_functors.h"
 #include "cpputil\not_fn.h"
+#include "cpputil\apply.h"
 #include "cpputil\exchange.h"
 #include "cpputil\forward.h"
 #include "cpputil\move.h"
