8e6fd7c15630ef1ae65a9e15e3a6459604c426c5
Edgar Rokian <edgar.rokyan@gmail.com>
2018-06-15 23:38:51 +0200

    Refine packer tests.

--- test/packer_test.cpp
+++ test/packer_test.cpp
@@ -1,28 +1,36 @@
 #include <gtest\gtest.h>
-#include <type_traits>
 #include <packer.h>
+#include "test_types.h"
 
-TEST(packer_test, test_is_contained_in)
+namespace test
 {
-    EXPECT_TRUE((cpputil::is_contained_in_v<void, void>));
-    EXPECT_TRUE((cpputil::is_contained_in_v<void, void, int>));
-    EXPECT_TRUE((cpputil::is_contained_in_v<void, void, int, double>));
+    TEST(packer_test, test_is_contained_in)
+    {
+        using cpputil::is_contained_in_v;
 
-    EXPECT_FALSE((cpputil::is_contained_in_v<void>));
-    EXPECT_FALSE((cpputil::is_contained_in_v<void, int>));
-    EXPECT_FALSE((cpputil::is_contained_in_v<void, int, double>));
-}
+        EXPECT_TRUE((is_contained_in_v<T0, T0>));
+        EXPECT_TRUE((is_contained_in_v<T0, T0, T1>));
+        EXPECT_TRUE((is_contained_in_v<T0, T0, T1, T2>));
 
-TEST(packer_test, test_contains_types)
-{
-    EXPECT_TRUE((cpputil::contains_types_v<cpputil::packer<void, int>, cpputil::packer<>>));
-    EXPECT_TRUE((cpputil::contains_types_v<cpputil::packer<void, int>, cpputil::packer<void>>));
-    EXPECT_TRUE((cpputil::contains_types_v<cpputil::packer<void, int>, cpputil::packer<int>>));
-    EXPECT_TRUE((cpputil::contains_types_v<cpputil::packer<void, int>, cpputil::packer<void, int>>));
-    EXPECT_TRUE((cpputil::contains_types_v<cpputil::packer<void, int>, cpputil::packer<int, void>>));
+        EXPECT_FALSE((is_contained_in_v<T0>));
+        EXPECT_FALSE((is_contained_in_v<T0, T1>));
+        EXPECT_FALSE((is_contained_in_v<T0, T1, T2>));
+        EXPECT_FALSE((is_contained_in_v<T0, T1, T2, T3>));
+    }
 
-    EXPECT_FALSE((cpputil::contains_types_v<cpputil::packer<void, int>, cpputil::packer<double>>));
-    EXPECT_FALSE((cpputil::contains_types_v<cpputil::packer<void, int>, cpputil::packer<void, double>>));
-    EXPECT_FALSE((cpputil::contains_types_v<cpputil::packer<void, int>, cpputil::packer<double, void>>));
-    EXPECT_FALSE((cpputil::contains_types_v<cpputil::packer<void, int>, cpputil::packer<double, void, int>>));
+    TEST(packer_test, test_contains_types)
+    {
+        using cpputil::contains_types_v;
+        using cpputil::packer;
+
+        EXPECT_TRUE((contains_types_v<packer<T0, T1>, packer<>>));
+        EXPECT_TRUE((contains_types_v<packer<T0, T1>, packer<T0>>));
+        EXPECT_TRUE((contains_types_v<packer<T0, T1>, packer<T1>>));
+        EXPECT_TRUE((contains_types_v<packer<T0, T1>, packer<T0, T1>>));
+
+        EXPECT_FALSE((contains_types_v<packer<T0, T1>, packer<T2>>));
+        EXPECT_FALSE((contains_types_v<packer<T0, T1>, packer<T0, T2>>));
+        EXPECT_FALSE((contains_types_v<packer<T0, T1>, packer<T2, T0>>));
+        EXPECT_FALSE((contains_types_v<packer<T0, T1>, packer<T0, T1, T2>>));
+    }
 }
