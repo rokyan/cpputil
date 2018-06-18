ca9f2b2612be24d436c9e8067e105e3059f51dec
Edgar Rokian <edgar.rokyan@gmail.com>
2018-06-18 21:39:50 +0200

    Add foreach_tuple_argument with basic tests.

--- include/cpputil/foreach_argument.h
+++ include/cpputil/foreach_argument.h
@@ -1,13 +1,37 @@
 #pragma once
 
+#include <type_traits>
+#include <utility>
+#include <tuple>
 #include <forward.h>
 
 namespace cpputil
 {
+    // foreach_argument implementation
 
     template<typename Func, typename... Args>
     void foreach_argument(Func&& f, Args&&... args)
     {
         (static_cast<void>(cpputil::forward<Func>(f)(cpputil::forward<Args>(args))), ...);
+    }
+
+    // foreach_tuple_argument implementation
+
+    template<typename Func, typename Tuple, std::size_t... Is>
+    void foreach_tuple_argument_impl(Func&& f, Tuple&& t, std::index_sequence<Is...>)
+    {
+        foreach_argument(
+            [&f](auto&&... elems) {
+                cpputil::forward<Func>(f)(cpputil::forward<decltype(elems)>(elems)...);
+            },
+            std::get<Is>(t)...);
+    }
+
+    template<typename Func, typename Tuple>
+    void foreach_tuple_argument(Func&& f, Tuple&& t)
+    {
+        using index_sequence_type = std::make_index_sequence<std::tuple_size_v<std::decay_t<Tuple>>>;
+
+        foreach_tuple_argument_impl(cpputil::forward<Func>(f), cpputil::forward<Tuple>(t), index_sequence_type{});
     }
 }
--- test/foreach_argument_test.cpp
+++ test/foreach_argument_test.cpp
@@ -14,4 +14,16 @@ namespace test
 
         EXPECT_EQ(sum, 15);
     }
+
+    TEST(foreach_argument_test, test_foreach_tuple_argument_basic_scenario)
+    {
+        int sum = 0;
+        auto sum_func = [&sum](auto x) {
+            sum += static_cast<int>(x);
+        };
+
+        cpputil::foreach_tuple_argument(sum_func, std::make_tuple(1, 2L, 3LL, 4.0f, 5.0));
+
+        EXPECT_EQ(sum, 15);
+    }
 }
