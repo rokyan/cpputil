baad205631752980a3b8e7f37940d2ec579f4c90
Edgar Rokian <edgar.rokyan@gmail.com>
2018-07-04 23:00:21 +0200

    Minor types fix.

--- test/foreach_argument_test.cpp
+++ test/foreach_argument_test.cpp
@@ -30,7 +30,7 @@ namespace test
 
         cpputil::foreach_argument(get_sum, 1, 2L, 3LL);
 
-        EXPECT_EQ(sum, 6);
+        EXPECT_EQ(sum, 6LL);
     }
 
     TEST(foreach_argument_test, test_foreach_argument_value_category)
@@ -41,8 +41,8 @@ namespace test
         cpputil::foreach_argument(ltb, 1, 2L, 3LL);
         cpputil::foreach_argument(cpputil::move(rtb), 1, 2L, 3LL);
 
-        EXPECT_EQ(ltb.value, 6);
-        EXPECT_EQ(rtb.value, 14);
+        EXPECT_EQ(ltb.value, 6LL);
+        EXPECT_EQ(rtb.value, 14LL);
     }
 
     TEST(foreach_argument_test, test_foreach_tuple_argument)
@@ -54,7 +54,7 @@ namespace test
 
         cpputil::foreach_tuple_argument(get_sum, std::make_tuple(1, 2L, 3LL));
 
-        EXPECT_EQ(sum, 6);
+        EXPECT_EQ(sum, 6LL);
     }
 
     TEST(foreach_argument_test, test_foreach_tuple_argument_value_category)
@@ -65,7 +65,7 @@ namespace test
         cpputil::foreach_tuple_argument(ltb, std::make_tuple(1, 2L, 3LL));
         cpputil::foreach_tuple_argument(cpputil::move(rtb), std::make_tuple(1, 2L, 3LL));
 
-        EXPECT_EQ(ltb.value, 6);
-        EXPECT_EQ(rtb.value, 14);
+        EXPECT_EQ(ltb.value, 6LL);
+        EXPECT_EQ(rtb.value, 14LL);
     }
 }
