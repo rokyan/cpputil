dec9de4f4d1a50784494ef89095fc33ab3d017ba
Edgar Rokian <edgar.rokyan@gmail.com>
2018-07-09 21:43:20 +0200

    Move foreach_tuple_argument_impl to the detail namespace.

--- include/cpputil/foreach_argument.hpp
+++ include/cpputil/foreach_argument.hpp
@@ -17,14 +17,17 @@ namespace cpputil
 
     // foreach_tuple_argument implementation
 
-    template<typename Func, typename Tuple, std::size_t... Is>
-    void foreach_tuple_argument_impl(Func&& f, Tuple&& t, std::index_sequence<Is...>)
+    namespace detail
     {
-        foreach_argument(
-            [&f](auto&&... elems) {
-                std::invoke(cpputil::forward<Func>(f), cpputil::forward<decltype(elems)>(elems)...);
-            },
-            std::get<Is>(t)...);
+        template<typename Func, typename Tuple, std::size_t... Is>
+        void foreach_tuple_argument_impl(Func&& f, Tuple&& t, std::index_sequence<Is...>)
+        {
+            foreach_argument(
+                [&f](auto&&... elems) {
+                    std::invoke(cpputil::forward<Func>(f), cpputil::forward<decltype(elems)>(elems)...);
+                },
+                std::get<Is>(t)...);
+        }
     }
 
     template<typename Func, typename Tuple>
@@ -32,6 +35,6 @@ namespace cpputil
     {
         using index_sequence_type = std::make_index_sequence<std::tuple_size_v<std::decay_t<Tuple>>>;
 
-        foreach_tuple_argument_impl(cpputil::forward<Func>(f), cpputil::forward<Tuple>(t), index_sequence_type{});
+        detail::foreach_tuple_argument_impl(cpputil::forward<Func>(f), cpputil::forward<Tuple>(t), index_sequence_type{});
     }
 }
