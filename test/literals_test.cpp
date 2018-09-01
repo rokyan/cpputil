fb90fc9ee626b69b761848a03e755c245cfeead6
Edgar Rokian <edgar.rokyan@gmail.com>
2018-09-01 11:20:26 +0200

    Fix copy-paste error at literals tests.

--- test/literals_test.cpp
+++ test/literals_test.cpp
@@ -15,6 +15,6 @@ namespace test
         auto from_double = 1.1_zu;
 
         EXPECT_SAME_TYPES(std::size_t, decltype(from_double));
-        EXPECT_EQ(std::size_t{ 1 }, from_int);
+        EXPECT_EQ(std::size_t{ 1 }, from_double);
     }
 }
