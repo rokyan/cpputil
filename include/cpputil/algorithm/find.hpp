bef93b80c9d43e342cfa68887350c86caabd5b80
Edgar Rokian <edgar.rokyan@gmail.com>
2018-10-14 20:33:39 +0200

    Add algorithms. Add find, find_if, find_if_not algorithms.

---
+++ include/cpputil/algorithm/find.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+namespace cpputil
+{
+    template<typename InputIterator, typename T>
+    constexpr InputIterator find(InputIterator first, InputIterator last, const T& value)
+    {
+        for (; first != last; ++first)
+        {
+            if (*first == value)
+            {
+                return first;
+            }
+        }
+
+        return first;
+    }
+
+    template<typename InputIterator, typename UnaryPredicate>
+    constexpr InputIterator find_if(InputIterator first, InputIterator last, UnaryPredicate predicate)
+    {
+        for (; first != last; ++first)
+        {
+            if (predicate(*first))
+            {
+                return first;
+            }
+        }
+
+        return first;
+    }
+
+    template<typename InputIterator, typename UnaryPredicate>
+    constexpr InputIterator find_if_not(InputIterator first, InputIterator last, UnaryPredicate predicate)
+    {
+        for (; first != last; ++first)
+        {
+            if (!predicate(*first))
+            {
+                return first;
+            }
+        }
+
+        return first;
+    }
+}
