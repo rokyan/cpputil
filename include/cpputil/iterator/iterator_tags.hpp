0cdd41582f43be59a4145d9e6283e027c3920ed0
Edgar Rokian <edgar.rokyan@gmail.com>
2018-10-17 22:03:24 +0200

    Add iterator tags. Formatting changes.

---
+++ include/cpputil/iterator/iterator_tags.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <iterator>
+
+namespace cpputil
+{
+    using input_iterator_tag = std::input_iterator_tag;
+    using output_iterator_tag = std::output_iterator_tag;
+    using forward_iterator_tag = std::forward_iterator_tag;
+    using bidirectional_iterator_tag = std::bidirectional_iterator_tag;
+    using random_access_iterator_tag = std::random_access_iterator_tag;
+}
