d8508fe5899d94a51723580c4d36774b70883ca6
Edgar Rokian <edgar.rokyan@gmail.com>
2018-10-18 23:38:11 +0200

    Add distance function with tests.

---
+++ include/cpputil/iterator/distance.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "iterator_tags.hpp"
+#include "iterator_traits.hpp"
+
+namespace cpputil
+{
+    namespace impl
+    {
+        template<typename InputIterator>
+        auto distance_impl(InputIterator first, InputIterator last, input_iterator_tag) ->
+            typename iterator_traits<InputIterator>::difference_type
+        {
+            typename iterator_traits<InputIterator>::difference_type difference = 0;
+
+            while (first != last)
+            {
+                ++difference;
+                ++first;
+            }
+
+            return difference;
+        }
+
+        template<typename InputIterator>
+        auto distance_impl(InputIterator first, InputIterator last, bidirectional_iterator_tag) ->
+            typename iterator_traits<InputIterator>::difference_type
+        {
+            return last - first;
+        }
+    }
+
+    template<typename InputIterator>
+    auto distance(InputIterator first, InputIterator last) ->
+        typename iterator_traits<InputIterator>::difference_type
+    {
+        return impl::distance_impl(first, last, typename iterator_traits<InputIterator>::iterator_category{});
+    }
+}
