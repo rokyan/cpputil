85dc43b6622afdcabcdd3c6e485ebad795cddf26
Edgar Rokian <edgar.rokyan@gmail.com>
2018-11-11 16:11:18 +0100

    Add all_of, any_of, none_of with tests.

---
+++ include/cpputil/algorithm/op_of.hpp
@@ -0,0 +1,30 @@
+#ifndef CPPUTIL_OP_OF_HPP
+#define CPPUTIL_OP_OF_HPP
+
+#include "find.hpp"
+
+namespace cpputil
+{
+    template<typename InputIterator, typename Predicate>
+    auto all_of(InputIterator first, InputIterator last, Predicate predicate)
+        -> bool
+    {
+        return cpputil::find_if_not(first, last, predicate) == last;
+    }
+
+    template<typename InputIterator, typename Predicate>
+    auto any_of(InputIterator first, InputIterator last, Predicate predicate)
+        -> bool
+    {
+        return cpputil::find_if(first, last, predicate) != last;
+    }
+
+    template<typename InputIterator, typename Predicate>
+    auto none_of(InputIterator first, InputIterator last, Predicate predicate)
+        -> bool
+    {
+        return cpputil::find_if(first, last, predicate) == last;
+    }
+}
+
+#endif // CPPUTIL_OP_OF_HPP
