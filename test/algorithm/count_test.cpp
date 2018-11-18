a457d1f6c016380149cf99f6cfe33cd1a4f552cd
Edgar Rokian <edgar.rokyan@gmail.com>
2018-11-18 19:43:16 +0100

    Minor changes in algorithms.

--- test/algorithm/count_test.cpp
+++ test/algorithm/count_test.cpp
@@ -4,40 +4,42 @@
 
 namespace test
 {
-    TEST(CountTest, TestEmptyRange)
-    {
-        using std::begin;
-        using std::end;
 
-        std::vector<int> data;
-        auto predicate = [](int value) { return value == 1; };
+TEST(CountTest, TestEmptyRange)
+{
+    using std::begin;
+    using std::end;
 
-        EXPECT_EQ(cpputil::count(begin(data), end(data), 1), 0);
-        EXPECT_EQ(cpputil::count_if(begin(data), end(data), predicate), 0);
-    }
+    std::vector<int> data;
+    auto predicate = [](int value) { return value == 1; };
 
-    TEST(CountTest, TestCountingExistingElement)
-    {
-        using std::begin;
-        using std::end;
-
-        std::vector<int> data{ 2, 4, 1, 2, 1 };
-        auto predicate = [](int value) { return value == 1; };
-
-        EXPECT_EQ(cpputil::count(begin(data), end(data), 1), 2);
-        EXPECT_EQ(cpputil::count_if(begin(data), end(data), predicate), 2);
-    }
-
-    TEST(CountTest, TestCountingNonExistingElement)
-    {
-        using std::begin;
-        using std::end;
-
-        std::vector<int> data{ 2, 4, 2 };
-        auto predicate = [](int value) { return value == 1; };
-
-        auto expected = std::end(data);
-        EXPECT_EQ(cpputil::count(begin(data), end(data), 1), 0);
-        EXPECT_EQ(cpputil::count_if(begin(data), end(data), predicate), 0);
-    }
+    EXPECT_EQ(cpputil::count(begin(data), end(data), 1), 0);
+    EXPECT_EQ(cpputil::count_if(begin(data), end(data), predicate), 0);
 }
+
+TEST(CountTest, TestCountingExistingElement)
+{
+    using std::begin;
+    using std::end;
+
+    std::vector<int> data{ 2, 4, 1, 2, 1 };
+    auto predicate = [](int value) { return value == 1; };
+
+    EXPECT_EQ(cpputil::count(begin(data), end(data), 1), 2);
+    EXPECT_EQ(cpputil::count_if(begin(data), end(data), predicate), 2);
+}
+
+TEST(CountTest, TestCountingNonExistingElement)
+{
+    using std::begin;
+    using std::end;
+
+    std::vector<int> data{ 2, 4, 2 };
+    auto predicate = [](int value) { return value == 1; };
+
+    auto expected = std::end(data);
+    EXPECT_EQ(cpputil::count(begin(data), end(data), 1), 0);
+    EXPECT_EQ(cpputil::count_if(begin(data), end(data), predicate), 0);
+}
+
+} // namespace test
--- test/algorithm/for_each_test.cpp
+++ test/algorithm/for_each_test.cpp
@@ -4,67 +4,66 @@
 
 namespace test
 {
-    struct Accumulator
-    {
-        void operator()(int value)
-        {
-            acc += value;
-        }
 
-        int acc = 0;
-    };
+struct accumulator
+{
+    void operator()(int value) noexcept { acc += value; }
 
-    TEST(ForEachTest, TestForEach)
-    {
-        using std::begin;
-        using std::end;
+    int acc = 0;
+};
 
-        std::vector<int> empty;
-        std::vector<int> data{ 1, 2, 3, 4, 5 };
+TEST(ForEachTest, TestForEach)
+{
+    using std::begin;
+    using std::end;
 
-        auto func = [](int& elem) { elem *= elem; };
+    std::vector<int> empty;
+    std::vector<int> data{ 1, 2, 3, 4, 5 };
 
-        cpputil::for_each(begin(empty), end(empty), func);
-        EXPECT_EQ(empty, std::vector<int>{});
+    auto func = [](int& elem) { elem *= elem; };
 
-        const std::vector<int> expected{ 1, 4, 9, 16, 25 };
-        cpputil::for_each(begin(data), end(data), func);
-        EXPECT_EQ(data, expected);
-    }
+    cpputil::for_each(begin(empty), end(empty), func);
+    EXPECT_EQ(empty, std::vector<int>{});
 
-    TEST(ForEachTest, TestForEachN)
-    {
-        using std::begin;
-        using std::end;
-
-        std::vector<int> data{ 0, 0, 0, 0, 0 };
-
-        auto func = [](int& elem) { ++elem; };
-
-        for (auto i = std::size_t{ 0 }; i <= data.size(); ++i)
-        {
-            cpputil::for_each_n(begin(data), i, func);
-        }
-
-        const std::vector<int> expected{ 5, 4, 3, 2, 1 };
-        EXPECT_EQ(data, expected);
-    }
-
-    TEST(ForEachTest, TestReturnValue)
-    {
-        using std::begin;
-        using std::end;
-
-        std::vector<int> data{ 1, 2, 3, 4, 5 };
-
-        Accumulator acc1;
-        Accumulator ret1 = cpputil::for_each(begin(data), end(data), acc1);
-
-        EXPECT_EQ(ret1.acc, 15);
-
-        Accumulator acc2;
-        Accumulator ret2 = cpputil::for_each_n(begin(data), data.size(), acc2);
-
-        EXPECT_EQ(ret2.acc, 15);
-    }
+    const std::vector<int> expected{ 1, 4, 9, 16, 25 };
+    cpputil::for_each(begin(data), end(data), func);
+    EXPECT_EQ(data, expected);
 }
+
+TEST(ForEachTest, TestForEachN)
+{
+    using std::begin;
+    using std::end;
+
+    std::vector<int> data{ 0, 0, 0, 0, 0 };
+
+    auto func = [](int& elem) { ++elem; };
+
+    for (auto i = std::size_t{ 0 }; i <= data.size(); ++i)
+    {
+        cpputil::for_each_n(begin(data), i, func);
+    }
+
+    const std::vector<int> expected{ 5, 4, 3, 2, 1 };
+    EXPECT_EQ(data, expected);
+}
+
+TEST(ForEachTest, TestReturnValue)
+{
+    using std::begin;
+    using std::end;
+
+    std::vector<int> data{ 1, 2, 3, 4, 5 };
+
+    accumulator acc1;
+    accumulator ret1 = cpputil::for_each(begin(data), end(data), acc1);
+
+    EXPECT_EQ(ret1.acc, 15);
+
+    accumulator acc2;
+    accumulator ret2 = cpputil::for_each_n(begin(data), data.size(), acc2);
+
+    EXPECT_EQ(ret2.acc, 15);
+}
+
+} // namespace test
