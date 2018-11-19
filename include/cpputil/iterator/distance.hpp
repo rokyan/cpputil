d58fe1c914957d2e665b82a55c122ea03faf1917
Edgar Rokian <edgar.rokyan@gmail.com>
2018-11-19 21:07:44 +0100

    Formatting fixes.

--- include/cpputil/iterator/distance.hpp
+++ include/cpputil/iterator/distance.hpp
@@ -1,39 +1,41 @@
-#ifndef CPPUTIL_DISTANCE_H
-#define CPPUTIL_DISTANCE_H
+#ifndef CPPUTIL_DISTANCE_HPP
+#define CPPUTIL_DISTANCE_HPP
 
 #include "iterator_tags.hpp"
 #include "iterator_traits.hpp"
 
 namespace cpputil
 {
-    template<typename InputIterator>
-    constexpr auto distance_impl(InputIterator first, InputIterator last, input_iterator_tag) ->
-        difference_type_t<InputIterator>
+
+template<typename InputIterator>
+constexpr auto distance_impl(InputIterator first, InputIterator last, input_iterator_tag) ->
+    difference_type_t<InputIterator>
+{
+    difference_type_t<InputIterator> difference = 0;
+
+    while (first != last)
     {
-        difference_type_t<InputIterator> difference = 0;
-
-        while (first != last)
-        {
-            ++difference;
-            ++first;
-        }
-
-        return difference;
+        ++difference;
+        ++first;
     }
 
-    template<typename InputIterator>
-    constexpr auto distance_impl(InputIterator first, InputIterator last, random_access_iterator_tag) ->
-        difference_type_t<InputIterator>
-    {
-        return last - first;
-    }
-
-    template<typename InputIterator>
-    constexpr auto distance(InputIterator first, InputIterator last) ->
-        difference_type_t<InputIterator>
-    {
-        return distance_impl(first, last, iterator_category_t<InputIterator>{});
-    }
+    return difference;
 }
 
-#endif // CPPUTIL_DISTANCE_H
+template<typename InputIterator>
+constexpr auto distance_impl(InputIterator first, InputIterator last, random_access_iterator_tag) ->
+    difference_type_t<InputIterator>
+{
+    return last - first;
+}
+
+template<typename InputIterator>
+constexpr auto distance(InputIterator first, InputIterator last) ->
+    difference_type_t<InputIterator>
+{
+    return distance_impl(first, last, iterator_category_t<InputIterator>{});
+}
+
+} // namespace cpputil
+
+#endif // CPPUTIL_DISTANCE_HPP
