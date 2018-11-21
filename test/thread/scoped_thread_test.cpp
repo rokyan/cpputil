f20415b13dbdbc3c05bf86b2701ecf2b725a2b39
Edgar Rokian <edgar.rokyan@gmail.com>
2018-11-21 20:45:00 +0100

    Formatting fixes.

--- test/thread/scoped_thread_test.cpp
+++ test/thread/scoped_thread_test.cpp
@@ -5,32 +5,35 @@
 
 namespace test
 {
-    using cpputil::scoped_thread;
 
-    TEST(ScopedThreadTest, TestGeneralClassProperties)
-    {
-        EXPECT_TRUE((std::is_constructible_v<scoped_thread, std::thread>)) <<
-            "scoped_thread is expected to be constructible with an rvalue argument of type std::thread";
+using cpputil::scoped_thread;
 
-        EXPECT_TRUE(std::is_move_constructible_v<scoped_thread>) <<
-            "scoped_thread is expected to be move-constructible";
-        EXPECT_TRUE(std::is_move_assignable_v<scoped_thread>) <<
-            "scoped_thread is expected to be move-assignable";
+TEST(ScopedThreadTest, TestGeneralClassProperties)
+{
+    EXPECT_TRUE((std::is_constructible_v<scoped_thread, std::thread>)) <<
+        "scoped_thread is expected to be constructible with an rvalue argument of type std::thread";
 
-        EXPECT_FALSE(std::is_copy_constructible_v<scoped_thread>) <<
-            "scoped_thread is expected not to be copy-constructible";
-        EXPECT_FALSE(std::is_copy_assignable_v<scoped_thread>) <<
-            "scoped_thread is expected not to be copy-assignable";
-    }
+    EXPECT_TRUE(std::is_move_constructible_v<scoped_thread>) <<
+        "scoped_thread is expected to be move-constructible";
+    EXPECT_TRUE(std::is_move_assignable_v<scoped_thread>) <<
+        "scoped_thread is expected to be move-assignable";
 
-    TEST(ScopedThreadTest, TestThrowingConstructor)
-    {
-        EXPECT_NO_THROW({
-            scoped_thread sc_thread(std::thread{ [] {} });
-        });
-
-        EXPECT_THROW({
-            scoped_thread sc_thread(std::thread{});
-        }, std::logic_error);
-    }
+    EXPECT_FALSE(std::is_copy_constructible_v<scoped_thread>) <<
+        "scoped_thread is expected not to be copy-constructible";
+    EXPECT_FALSE(std::is_copy_assignable_v<scoped_thread>) <<
+        "scoped_thread is expected not to be copy-assignable";
 }
+
+TEST(ScopedThreadTest, TestThrowingConstructor)
+{
+    EXPECT_NO_THROW({
+        scoped_thread sc_thread(std::thread{ [] {} });
+    });
+
+    EXPECT_THROW({
+        scoped_thread sc_thread(std::thread{});
+    }, std::logic_error);
+}
+
+} // namespace test
+
