301d8a1bc322e5d603968aa0ab9ed8d145a067ef
Edgar Rokian <edgar.rokyan@gmail.com>
2018-11-23 19:41:11 +0100

    Fix tests.

--- test/iterator/next_test.cpp
+++ test/iterator/next_test.cpp
@@ -27,20 +27,20 @@ TEST(NextTest, TestAddition)
     input_iterator_mock it{ 0 };
     it.set_increment_callback(callback);
 
-    const input_iterator_mock ret_it_1 = cpputil::next(it, 1);
+    it = cpputil::next(it, 1);
 
     EXPECT_EQ(increment_counter, 1);
-    EXPECT_EQ(ret_it_1.get_value(), 1);
+    EXPECT_EQ(it.get_value(), 1);
 
-    const input_iterator_mock ret_it_2 = cpputil::next(it, 2);
+    it = cpputil::next(it, 2);
 
     EXPECT_EQ(increment_counter, 3);
-    EXPECT_EQ(ret_it_2.get_value(), 3);
+    EXPECT_EQ(it.get_value(), 3);
 
-    const input_iterator_mock ret_it_3 = cpputil::next(it, 3);
+    it = cpputil::next(it, 3);
 
     EXPECT_EQ(increment_counter, 6);
-    EXPECT_EQ(ret_it_3.get_value(), 6);
+    EXPECT_EQ(it.get_value(), 6);
 }
 
 } // namespace test
--- test/iterator/prev_test.cpp
+++ test/iterator/prev_test.cpp
@@ -27,20 +27,20 @@ TEST(PrevTest, TestSubtraction)
     bidirectional_iterator_mock it{ 0 };
     it.set_decrement_callback(callback);
 
-    const bidirectional_iterator_mock ret_it_1 = cpputil::prev(it, 1);
+    it = cpputil::prev(it, 1);
 
     EXPECT_EQ(decrement_counter, 1);
-    EXPECT_EQ(ret_it_1.get_value(), -1);
+    EXPECT_EQ(it.get_value(), -1);
 
-    const bidirectional_iterator_mock ret_it_2 = cpputil::prev(it, 2);
+    it = cpputil::prev(it, 2);
 
     EXPECT_EQ(decrement_counter, 3);
-    EXPECT_EQ(ret_it_2.get_value(), -3);
+    EXPECT_EQ(it.get_value(), -3);
 
-    const bidirectional_iterator_mock ret_it_3 = cpputil::prev(it, 3);
+    it = cpputil::prev(it, 3);
 
     EXPECT_EQ(decrement_counter, 6);
-    EXPECT_EQ(ret_it_3.get_value(), 6);
+    EXPECT_EQ(it.get_value(), -6);
 }
 
 } // namespace test
