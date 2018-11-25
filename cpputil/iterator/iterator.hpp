2e4382574595b45517e68afd242b778691259547
Edgar Rokian <edgar.rokyan@gmail.com>
2018-11-25 19:19:23 +0100

    Fix inclusion in iterator.hpp.

--- cpputil/iterator/iterator.hpp
+++ cpputil/iterator/iterator.hpp
@@ -1,11 +1,8 @@
 #ifndef CPPUTIL_ITERATOR_HPP
 #define CPPUTIL_ITERATOR_HPP
 
-#include "advance.hpp"
-#include "distance.hpp"
 #include "iterator_tags.hpp"
+#include "iterator_operations.hpp"
 #include "iterator_traits.hpp"
-#include "next.hpp"
-#include "prev.hpp"
 
 #endif // CPPUTIL_ITERATOR_HPP
