cf49679d7005399cbf53d0bd11e874c67c8b236c
Edgar Rokian <edgar.rokyan@gmail.com>
2018-12-02 20:25:25 +0100

    Fix formatting for literals header and tests.

--- cpputil/utility/literals.hpp
+++ cpputil/utility/literals.hpp
@@ -5,22 +5,24 @@
 
 namespace cpputil
 {
-    namespace literals
+
+namespace literals
+{
+    /**
+     * Based on http://open-std.org/JTC1/SC22/WG21/docs/papers/2016/p0330r0.pdf
+     */
+
+    constexpr std::size_t operator"" _zu(unsigned long long value)
     {
-        /**
-         * Based on http://open-std.org/JTC1/SC22/WG21/docs/papers/2016/p0330r0.pdf
-         */
+        return static_cast<std::size_t>(value);
+    }
 
-        constexpr std::size_t operator"" _zu(unsigned long long value)
-        {
-            return static_cast<std::size_t>(value);
-        }
-
-        constexpr std::size_t operator"" _zu(long double value)
-        {
-            return static_cast<std::size_t>(value);
-        }
+    constexpr std::size_t operator"" _zu(long double value)
+    {
+        return static_cast<std::size_t>(value);
     }
 }
+
+} // namespace cpputil
 
 #endif //CPPUTIL_LITERALS_HPP
