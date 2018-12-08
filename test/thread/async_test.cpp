c51a963e02a5572361a1b7fce6257618b0c396aa
Edgar Rokian <edgar.rokyan@gmail.com>
2018-12-08 16:28:20 +0100

    Remove redundant const specifier.

--- test/thread/async_test.cpp
+++ test/thread/async_test.cpp
@@ -8,8 +8,8 @@ TEST(AsyncTest, TestAlwaysAsync)
 {
     auto sum = [](int x, int y) { return x + y; };
 
-    const auto sum_1 = cpputil::always_async(sum, 1, 2);
-    const auto sum_2 = cpputil::always_async(sum, 3, 4);
+     auto sum_1 = cpputil::always_async(sum, 1, 2);
+     auto sum_2 = cpputil::always_async(sum, 3, 4);
 
     EXPECT_EQ(sum_1.get() + sum_2.get(), 10);
 }
