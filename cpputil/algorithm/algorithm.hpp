012837c55f5468f3570419e2773f640ef2e66823
Edgar Rokian <edgar.rokyan@gmail.com>
2019-03-06 23:36:21 +0100

    Minor refactoring.

--- cpputil/algorithm/algorithm.hpp
+++ cpputil/algorithm/algorithm.hpp
@@ -1,15 +1,7 @@
 #ifndef CPPUTIL_ALGORITHM_HPP
 #define CPPUTIL_ALGORITHM_HPP
 
-#include "adjacent_find.hpp"
-#include "count.hpp"
-#include "find.hpp"
-#include "find_first_of.hpp"
-#include "find_end.hpp"
-#include "for_each.hpp"
-#include "mismatch.hpp"
 #include "minmax_ops.hpp"
-#include "range_check.hpp"
-#include "search.hpp"
+#include "non_md_seq_ops.hpp"
 
 #endif // CPPUTIL_ALGORITHM_HPP
