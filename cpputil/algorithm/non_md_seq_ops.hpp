edb3ca83ea4320f4939f9dcfd32ec13a1439de38
Edgar Rokian <edgar.rokyan@gmail.com>
2019-03-07 19:47:46 +0100

    Fix g++ build.

--- cpputil/algorithm/non_md_seq_ops.hpp
+++ cpputil/algorithm/non_md_seq_ops.hpp
@@ -7,6 +7,52 @@
 namespace cpputil
 {
 
+template<typename ForwardIterator1, typename ForwardIterator2, typename Predicate>
+constexpr auto search_impl(ForwardIterator1 first1, ForwardIterator1 last1,
+    ForwardIterator2 first2, ForwardIterator2 last2, Predicate pred) -> ForwardIterator1
+{
+    for (; first1 != last1; ++first1)
+    {
+        ForwardIterator1 first_range_it = first1;
+
+        for (ForwardIterator2 second_range_it = first2; ;
+            ++first_range_it, static_cast<void>(++second_range_it))
+        {
+            if (second_range_it == last2)
+            {
+                return first1;
+            }
+
+            if (first_range_it == last1)
+            {
+                return last1;
+            }
+
+            if (!pred(first_range_it, second_range_it))
+            {
+                break;
+            }
+        }
+
+    }
+
+    return first1;
+}
+
+template<typename ForwardIterator1, typename ForwardIterator2>
+constexpr auto search(ForwardIterator1 first1, ForwardIterator1 last1,
+    ForwardIterator2 first2, ForwardIterator2 last2) -> ForwardIterator1
+{
+    return cpputil::search_impl(first1, last1, first2, last2, make_iter_eq_to_iter());
+}
+
+template<typename ForwardIterator1, typename ForwardIterator2, typename Predicate>
+constexpr auto search(ForwardIterator1 first1, ForwardIterator1 last1,
+    ForwardIterator2 first2, ForwardIterator2 last2, Predicate pred) -> ForwardIterator1
+{
+    return cpputil::search_impl(first1, last1, first2, last2, make_iter_comp_to_iter(pred));
+}
+
 template<typename InputIterator, typename Predicate>
 constexpr auto find_impl(InputIterator first, InputIterator last, Predicate pred) -> InputIterator
 {
@@ -147,52 +193,6 @@ template<typename ForwardIterator, typename Predicate>
 constexpr auto adjacent_find(ForwardIterator first, ForwardIterator last, Predicate pred) -> ForwardIterator
 {
     return cpputil::adjacent_find_impl(first, last, make_iter_comp_to_iter(pred));
-}
-
-template<typename ForwardIterator1, typename ForwardIterator2, typename Predicate>
-constexpr auto search_impl(ForwardIterator1 first1, ForwardIterator1 last1,
-    ForwardIterator2 first2, ForwardIterator2 last2, Predicate pred) -> ForwardIterator1
-{
-    for (; first1 != last1; ++first1)
-    {
-        ForwardIterator1 first_range_it = first1;
-
-        for (ForwardIterator2 second_range_it = first2; ;
-            ++first_range_it, static_cast<void>(++second_range_it))
-        {
-            if (second_range_it == last2)
-            {
-                return first1;
-            }
-
-            if (first_range_it == last1)
-            {
-                return last1;
-            }
-
-            if (!pred(first_range_it, second_range_it))
-            {
-                break;
-            }
-        }
-
-    }
-
-    return first1;
-}
-
-template<typename ForwardIterator1, typename ForwardIterator2>
-constexpr auto search(ForwardIterator1 first1, ForwardIterator1 last1,
-    ForwardIterator2 first2, ForwardIterator2 last2) -> ForwardIterator1
-{
-    return cpputil::search_impl(first1, last1, first2, last2, make_iter_eq_to_iter());
-}
-
-template<typename ForwardIterator1, typename ForwardIterator2, typename Predicate>
-constexpr auto search(ForwardIterator1 first1, ForwardIterator1 last1,
-    ForwardIterator2 first2, ForwardIterator2 last2, Predicate pred) -> ForwardIterator1
-{
-    return cpputil::search_impl(first1, last1, first2, last2, make_iter_comp_to_iter(pred));
 }
 
 template<typename InputIterator, typename Predicate>
