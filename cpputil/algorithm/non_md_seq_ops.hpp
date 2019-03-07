f560f27259c3884b3bbb9bc2edd64c6a34e3b146
Edgar Rokian <edgar.rokyan@gmail.com>
2019-03-07 21:06:26 +0100

    Formatting fixes.

--- cpputil/algorithm/non_md_seq_ops.hpp
+++ cpputil/algorithm/non_md_seq_ops.hpp
@@ -3,20 +3,19 @@
 
 #include "predicates.hpp"
 #include <iterator.hpp>
+#include <utility> // for std::pair and std::make_pair
 
 namespace cpputil
 {
 
 template<typename ForwardIterator1, typename ForwardIterator2, typename Predicate>
-constexpr auto search_impl(ForwardIterator1 first1, ForwardIterator1 last1,
-    ForwardIterator2 first2, ForwardIterator2 last2, Predicate pred) -> ForwardIterator1
+constexpr ForwardIterator1 search_impl(ForwardIterator1 first1, ForwardIterator1 last1, ForwardIterator2 first2, ForwardIterator2 last2, Predicate pred)
 {
     for (; first1 != last1; ++first1)
     {
         ForwardIterator1 first_range_it = first1;
 
-        for (ForwardIterator2 second_range_it = first2; ;
-            ++first_range_it, static_cast<void>(++second_range_it))
+        for (ForwardIterator2 second_range_it = first2; ; ++first_range_it, static_cast<void>(++second_range_it))
         {
             if (second_range_it == last2)
             {
@@ -40,51 +39,51 @@ constexpr auto search_impl(ForwardIterator1 first1, ForwardIterator1 last1,
 }
 
 template<typename ForwardIterator1, typename ForwardIterator2>
-constexpr auto search(ForwardIterator1 first1, ForwardIterator1 last1,
-    ForwardIterator2 first2, ForwardIterator2 last2) -> ForwardIterator1
+constexpr ForwardIterator1 search(ForwardIterator1 first1, ForwardIterator1 last1, ForwardIterator2 first2, ForwardIterator2 last2)
 {
     return cpputil::search_impl(first1, last1, first2, last2, make_iter_eq_to_iter());
 }
 
 template<typename ForwardIterator1, typename ForwardIterator2, typename Predicate>
-constexpr auto search(ForwardIterator1 first1, ForwardIterator1 last1,
-    ForwardIterator2 first2, ForwardIterator2 last2, Predicate pred) -> ForwardIterator1
+constexpr ForwardIterator1 search(ForwardIterator1 first1, ForwardIterator1 last1, ForwardIterator2 first2, ForwardIterator2 last2, Predicate pred)
 {
     return cpputil::search_impl(first1, last1, first2, last2, make_iter_comp_to_iter(pred));
 }
 
 template<typename InputIterator, typename Predicate>
-constexpr auto find_impl(InputIterator first, InputIterator last, Predicate pred) -> InputIterator
+constexpr InputIterator find_impl(InputIterator first, InputIterator last, Predicate pred)
 {
     for (; first != last; ++first)
     {
-        if (pred(first)) { return first; }
+        if (pred(first))
+        {
+            return first;
+        }
     }
 
     return last;
 }
 
 template<typename InputIterator, typename T>
-constexpr auto find(InputIterator first, InputIterator last, const T& value) -> InputIterator
+constexpr InputIterator find(InputIterator first, InputIterator last, const T& value)
 {
     return cpputil::find_impl(first, last, make_iter_eq_to_val(value));
 }
 
 template<typename InputIterator, typename UnaryPredicate>
-constexpr auto find_if(InputIterator first, InputIterator last, UnaryPredicate pred) -> InputIterator
+constexpr InputIterator find_if(InputIterator first, InputIterator last, UnaryPredicate pred)
 {
     return cpputil::find_impl(first, last, make_iter_pred(pred));
 }
 
 template<typename InputIterator, typename UnaryPredicate>
-constexpr auto find_if_not(InputIterator first, InputIterator last, UnaryPredicate pred) -> InputIterator
+constexpr InputIterator find_if_not(InputIterator first, InputIterator last, UnaryPredicate pred)
 {
     return cpputil::find_impl(first, last, make_iter_neg_pred(pred));
 }
 
 template<typename ForwardIterator1, typename ForwardIterator2, typename Predicate>
-constexpr auto find_end_impl(ForwardIterator1 first1, ForwardIterator1 last1,
-    ForwardIterator2 first2, ForwardIterator2 last2, Predicate pred) -> ForwardIterator1
+constexpr ForwardIterator1 find_end_impl(ForwardIterator1 first1, ForwardIterator1 last1, ForwardIterator2 first2, ForwardIterator2 last2, Predicate pred)
 {
     if (first2 == last2)
     {
@@ -113,22 +112,19 @@ constexpr auto find_end_impl(ForwardIterator1 first1, ForwardIterator1 last1,
 }
 
 template<typename ForwardIterator1, typename ForwardIterator2>
-constexpr auto find_end(ForwardIterator1 first1, ForwardIterator1 last1,
-    ForwardIterator2 first2, ForwardIterator2 last2) -> ForwardIterator1
+constexpr ForwardIterator1 find_end(ForwardIterator1 first1, ForwardIterator1 last1, ForwardIterator2 first2, ForwardIterator2 last2)
 {
     return cpputil::find_end_impl(first1, last1, first2, last2, make_iter_eq_to_iter());
 }
 
 template<typename ForwardIterator1, typename ForwardIterator2, typename Predicate>
-constexpr auto find_end(ForwardIterator1 first1, ForwardIterator1 last1,
-    ForwardIterator2 first2, ForwardIterator2 last2, Predicate pred) -> ForwardIterator1
+constexpr ForwardIterator2 find_end(ForwardIterator1 first1, ForwardIterator1 last1, ForwardIterator2 first2, ForwardIterator2 last2, Predicate pred)
 {
     return cpputil::find_end_impl(first1, last1, first2, last2, make_iter_comp_to_iter(pred));
 }
 
 template<typename InputIterator, typename ForwardIterator, typename Predicate>
-constexpr auto find_first_of_impl(InputIterator first1, InputIterator last1,
-    ForwardIterator first2, ForwardIterator last2, Predicate pred) -> InputIterator
+constexpr InputIterator find_first_of_impl(InputIterator first1, InputIterator last1, ForwardIterator first2, ForwardIterator last2, Predicate pred)
 {
     for (; first1 != last1; ++first1)
     {
@@ -145,23 +141,19 @@ constexpr auto find_first_of_impl(InputIterator first1, InputIterator last1,
 }
 
 template<typename InputIterator, typename ForwardIterator>
-constexpr auto find_first_of(InputIterator first1, InputIterator last1,
-    ForwardIterator first2, ForwardIterator last2) -> InputIterator
+constexpr InputIterator find_first_of(InputIterator first1, InputIterator last1, ForwardIterator first2, ForwardIterator last2)
 {
-    return cpputil::find_first_of_impl(first1, last1, first2, last2,
-        make_iter_eq_to_iter());
+    return cpputil::find_first_of_impl(first1, last1, first2, last2, make_iter_eq_to_iter());
 }
 
 template<typename InputIterator, typename ForwardIterator, typename BinaryPredicate>
-constexpr auto find_first_of(InputIterator first1, InputIterator last1,
-    ForwardIterator first2, ForwardIterator last2, BinaryPredicate pred) -> InputIterator
+constexpr InputIterator find_first_of(InputIterator first1, InputIterator last1, ForwardIterator first2, ForwardIterator last2, BinaryPredicate pred)
 {
-    return cpputil::find_first_of_impl(first1, last1, first2, last2,
-        make_iter_comp_to_iter(pred));
+    return cpputil::find_first_of_impl(first1, last1, first2, last2, make_iter_comp_to_iter(pred));
 }
 
 template<typename ForwardIterator, typename Predicate>
-constexpr auto adjacent_find_impl(ForwardIterator first, ForwardIterator last, Predicate pred) -> ForwardIterator
+constexpr ForwardIterator adjacent_find_impl(ForwardIterator first, ForwardIterator last, Predicate pred)
 {
     if (first == last)
     {
@@ -184,37 +176,37 @@ constexpr auto adjacent_find_impl(ForwardIterator first, ForwardIterator last, P
 }
 
 template<typename ForwardIterator>
-constexpr auto adjacent_find(ForwardIterator first, ForwardIterator last) -> ForwardIterator
+constexpr ForwardIterator adjacent_find(ForwardIterator first, ForwardIterator last)
 {
     return cpputil::adjacent_find_impl(first, last, make_iter_eq_to_iter());
 }
 
 template<typename ForwardIterator, typename Predicate>
-constexpr auto adjacent_find(ForwardIterator first, ForwardIterator last, Predicate pred) -> ForwardIterator
+constexpr ForwardIterator adjacent_find(ForwardIterator first, ForwardIterator last, Predicate pred)
 {
     return cpputil::adjacent_find_impl(first, last, make_iter_comp_to_iter(pred));
 }
 
 template<typename InputIterator, typename Predicate>
-constexpr auto all_of(InputIterator first, InputIterator last, Predicate predicate) -> bool
+constexpr bool all_of(InputIterator first, InputIterator last, Predicate predicate)
 {
     return cpputil::find_if_not(first, last, predicate) == last;
 }
 
 template<typename InputIterator, typename Predicate>
-constexpr auto any_of(InputIterator first, InputIterator last, Predicate predicate) -> bool
+constexpr bool any_of(InputIterator first, InputIterator last, Predicate predicate)
 {
     return cpputil::find_if(first, last, predicate) != last;
 }
 
 template<typename InputIterator, typename Predicate>
-constexpr auto none_of(InputIterator first, InputIterator last, Predicate predicate) -> bool
+constexpr bool none_of(InputIterator first, InputIterator last, Predicate predicate)
 {
     return cpputil::find_if(first, last, predicate) == last;
 }
 
 template<typename InputIterator, typename Function>
-constexpr auto for_each(InputIterator first, InputIterator last, Function func) -> Function
+constexpr Function for_each(InputIterator first, InputIterator last, Function func)
 {
     for (; first < last; ++first)
     {
@@ -225,7 +217,7 @@ constexpr auto for_each(InputIterator first, InputIterator last, Function func) 
 }
 
 template<typename InputIterator, typename T, typename Function>
-constexpr auto for_each_n(InputIterator first, T n, Function func) -> Function
+constexpr Function for_each_n(InputIterator first, T n, Function func)
 {
     for (T i = 0; i < n; ++first, static_cast<void>(++i))
     {
@@ -236,8 +228,7 @@ constexpr auto for_each_n(InputIterator first, T n, Function func) -> Function
 }
 
 template<typename InputIterator, typename Predicate>
-constexpr auto count_impl(InputIterator first, InputIterator last, Predicate pred)
-    -> cpputil::difference_type_t<InputIterator>
+constexpr cpputil::difference_type_t<InputIterator>count_impl(InputIterator first, InputIterator last, Predicate pred)
 {
     cpputil::difference_type_t<InputIterator> total = 0;
 
@@ -250,67 +241,59 @@ constexpr auto count_impl(InputIterator first, InputIterator last, Predicate pre
 }
 
 template<typename InputIterator, typename T>
-constexpr auto count(InputIterator first, InputIterator last, const T& value)
-    -> cpputil::difference_type_t<InputIterator>
+constexpr cpputil::difference_type_t<InputIterator> count(InputIterator first, InputIterator last, const T& value)
 {
     return cpputil::count_impl(first, last, make_iter_eq_to_val(value));
 }
 
 template<typename InputIterator, typename Predicate>
-constexpr auto count_if(InputIterator first, InputIterator last, Predicate pred)
-    -> cpputil::difference_type_t<InputIterator>
+constexpr cpputil::difference_type_t<InputIterator> count_if(InputIterator first, InputIterator last, Predicate pred)
 {
     return cpputil::count_impl(first, last, make_iter_pred(pred));
 }
 
 template<typename InputIterator1, typename InputIterator2, typename Predicate>
-constexpr auto mismatch_impl(InputIterator1 first1, InputIterator1 last1,
-    InputIterator2 first2, Predicate pred) -> std::pair<InputIterator1, InputIterator2>
+constexpr std::pair<InputIterator1, InputIterator2> mismatch_impl(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, Predicate pred)
 {
     while (first1 != last1 && pred(first1 ,first2))
     {
         ++first1; ++first2;
     }
 
-    return std::pair{ first1, first2 };
+    return std::make_pair(first1, first2);
 }
 
 template<typename InputIterator1, typename InputIterator2, typename Predicate>
-constexpr auto mismatch_impl(InputIterator1 first1, InputIterator1 last1,
-    InputIterator2 first2, InputIterator2 last2, Predicate pred) -> std::pair<InputIterator1, InputIterator2>
+constexpr std::pair<InputIterator1, InputIterator2>mismatch_impl(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, Predicate pred)
 {
     while (first1 != last1 && first2 != last2 && pred(first1 ,first2))
     {
         ++first1; ++first2;
     }
 
-    return std::pair{ first1, first2 };
+    return std::make_pair(first1, first2);
 }
 
 template<typename InputIterator1, typename InputIterator2>
-constexpr auto mismatch(InputIterator1 first1, InputIterator1 last1,
-    InputIterator2 first2) -> std::pair<InputIterator1, InputIterator2>
+constexpr std::pair<InputIterator1, InputIterator2>mismatch(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
 {
     return cpputil::mismatch_impl(first1, last1, first2, make_iter_eq_to_iter());
 }
 
 template<typename InputIterator1, typename InputIterator2, typename BinaryPredicate>
-constexpr auto mismatch(InputIterator1 first1, InputIterator1 last1,
-    InputIterator2 first2, BinaryPredicate pred) -> std::pair<InputIterator1, InputIterator2>
+constexpr std::pair<InputIterator1, InputIterator2>mismatch(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred)
 {
     return cpputil::mismatch_impl(first1, last1, first2, make_iter_comp_to_iter(pred));
 }
 
 template<typename InputIterator1, typename InputIterator2>
-constexpr auto mismatch(InputIterator1 first1, InputIterator1 last1,
-    InputIterator2 first2, InputIterator2 last2) -> std::pair<InputIterator1, InputIterator2>
+constexpr std::pair<InputIterator1, InputIterator2>mismatch(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
 {
     return cpputil::mismatch_impl(first1, last1, first2, last2, make_iter_eq_to_iter());
 }
 
 template<typename InputIterator1, typename InputIterator2, typename BinaryPredicate>
-constexpr auto mismatch(InputIterator1 first1, InputIterator1 last1,
-    InputIterator2 first2, InputIterator2 last2, BinaryPredicate pred) -> std::pair<InputIterator1, InputIterator2>
+constexpr std::pair<InputIterator1, InputIterator2>mismatch(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, BinaryPredicate pred)
 {
     return cpputil::mismatch_impl(first1, last1, first2, last2, make_iter_comp_to_iter(pred));
 }
