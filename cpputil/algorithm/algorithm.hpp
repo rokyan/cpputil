b5737c1f11fec6fb77c8a94ed7e1587490a0e5ff
Edgar Rokian <edgar.rokyan@gmail.com>
2019-03-11 21:43:41 +0100

    Reshuffle inclusions.

--- cpputil/algorithm/algorithm.hpp
+++ cpputil/algorithm/algorithm.hpp
@@ -1,7 +1,7 @@
 #ifndef CPPUTIL_ALGORITHM_HPP
 #define CPPUTIL_ALGORITHM_HPP
 
-#include "minmax_ops.hpp"
 #include "non_md_seq_ops.hpp"
+#include "minmax_ops.hpp"
 
 #endif // CPPUTIL_ALGORITHM_HPP
