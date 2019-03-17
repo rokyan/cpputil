ec00da634a365431d87d666630eb32adf9c19968
Edgar Rokian <edgar.rokyan@gmail.com>
2019-03-17 20:51:19 +0100

    Minor formatting fixes.

--- cpputil/utility/apply.hpp
+++ cpputil/utility/apply.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
-#include <forward.hpp>
 #include <integer_sequence.hpp>
+#include <forward.hpp>
+#include <traits.hpp>
 
 #include <utility> // for std::tuple.
 #include <functional> // for std::invoke.
@@ -18,10 +19,9 @@ decltype(auto) apply_impl(Func&& func, Tuple&& tuple_of_args, cpputil::index_seq
 template<typename Func, typename Tuple>
 decltype(auto) apply(Func&& func, Tuple&& tuple_of_args)
 {
-    using index_sequence_type = cpputil::make_index_sequence<std::tuple_size_v<std::decay_t<Tuple>>>;
+    using index_sequence_type = cpputil::make_index_sequence<std::tuple_size_v<traits::decay_t<Tuple>>>;
 
-    return cpputil::apply_impl(cpputil::forward<Func>(func),
-        cpputil::forward<Tuple>(tuple_of_args), index_sequence_type{});
+    return cpputil::apply_impl(cpputil::forward<Func>(func), cpputil::forward<Tuple>(tuple_of_args), index_sequence_type{});
 }
 
 } // namespace cpputil
