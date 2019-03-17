4dbd3443e586ce11a79a89d31c462c0693601c95
Edgar Rokian <edgar.rokyan@gmail.com>
2019-03-17 21:16:34 +0100

    Ooops :).

--- cpputil/utility/forward.hpp
+++ cpputil/utility/forward.hpp
@@ -6,13 +6,13 @@ namespace cpputil
 {
 
 template<typename T>
-constexpr T&& forward(traits::remove_reference_t<T>& t) noexcept
+constexpr T&& forward(cpputil::remove_reference_t<T>& t) noexcept
 {
     return static_cast<T&&>(t);
 }
 
 template<typename T>
-constexpr T&& forward(traits::remove_reference_t<T>&& t) noexcept
+constexpr T&& forward(cpputil::remove_reference_t<T>&& t) noexcept
 {
     return static_cast<T&&>(t);
 }
